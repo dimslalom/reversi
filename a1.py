074449638fa42570871246e2034bf516eca6a607
dimslalom <dimasgisthaa@email.com>
2025-04-15 19:29:15 +1000

    initial commit

---
+++ a1.py
@@ -0,0 +1,19 @@
+# DO NOT modify or add any import statements
+from support import *
+
+# Name:
+# Student Number: 
+# Favorite Marsupial: 
+# -----------------------------------------------------------------------------
+
+# Define your functions here (start with def num_hours() -> float)
+
+
+def main() -> None:
+    """
+    The main function (You should write a better docstring!)
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ support.py
@@ -0,0 +1,18 @@
+BOARD_SIZE = 8
+
+PLAYER_1 = "O"
+PLAYER_2 = "X"
+EMPTY = "+"
+
+VERTICAL_SEPARATOR = "|"
+HORIZONTAL_SEPARATOR = "-"
+
+WELCOME_MESSAGE = "Welcome to Reversi!"
+HELP_MESSAGE = """
+[A-H][1-8]: Place piece at specified square.
+H/h: Display help message.
+Q/q: Quit current game.
+"""
+MOVE_PROMPT = "Please enter move (Or H for help): "
+DRAW_TEXT = "It's a draw! Now that is impressive."
+PLAY_AGAIN_PROMPT = "Would you like to play again? (y/n): "
